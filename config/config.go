d59ac2791d71284405afbeff05cc0ec156069e2c
Debkanchan Samadder <debu.samadder@gmail.com>
2023-08-17 21:15:46 +0530

    fix: config

--- config/config.go
+++ config/config.go
@@ -1,7 +1,8 @@
 package config
 
 type EnvStruct struct {
-	Debug                 bool   `env:"DEBUG" env-description:"dev or prod" env-default:"false"`
+	Production            bool   `env:"PRODUCTION" env-description:"dev or prod" env-default:"true"`
+	LogDebug              bool   `env:"LOG_DEBUG" env-description:"should log at debug level" env-default:"false"`
 	Port                  int32  `env:"PORT" env-description:"server port" env-default:"50051"`
 	Project_Id            string `env:"PROJECT_ID" env-description:"firebase project id" env-default:"NO_PROJECT"`
 	Firebase_Database_url string `env:"FIREBASE_DATABASE_URL" env-description:"firebase database url" env-default:"NO_DATABASE_URL"`
